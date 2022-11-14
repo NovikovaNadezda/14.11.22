151fd497ce9ff156069b40836c070371fbf5a6b5
NovikovaNadezda <114093562+NovikovaNadezda@users.noreply.github.com>
2022-11-14 18:19:36 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+int main() {
+	srand(time(nullptr));
+	//1. Задан массив A, содержащий 12 целых случайных чисел от - 5 до 5. Найти сумму элементов массива.
+	int mas_a[12] = { -5,-4,-3,-2,-1,0,1,2,3,4,5 };
+	while (mas_a[ =!0])
+	{
+
+	}
+	}
+}
